865f8d7b5ed3b2844c98bdf3504dd3e30ab92bd2
Dimitrios Vlastaras <dimitrios@cryodev.se>
2011-04-18 01:08:01 +0000

    adfads

--- src/message_handler.h
+++ src/message_handler.h
@@ -1,11 +1,39 @@
 #ifndef MESSAGE_HANDLER_H
 #define MESSAGE_HANDLER_H
 
+#include "clientserver/server.h"
+#include "clientserver/connection.h"
+#include "clientserver/connectionclosedexception.h"
+
+#include <string>
+
 class MessageHandler {
 public:
     
-private:
+    MessageHandler(client_server::Connection* c) : conn(c) { }
     
+    void sendCode(int code);
+    
+    void sendInt(int value);
+    
+    void sendIntParameter(int param);
+    
+    void sendStringParameter(std::string param);
+    
+    int recvCore();
+    
+    int recvInt();
+    
+    int recvIntParameter();
+    
+    std::string recvStringParameter();
+    
+private:
+    void sendByte(int code);
+    
+    int recvByte();
+    
+    client_server::Connection* conn;
 
 };
 
